7a7c07a8725418a5e16c113399c79c7e16fea648
elias-ssouza <elias.ssouza@hotmail.com>
2022-11-24 18:01:16 -0300

    finalização do setup profile

--- app/src/main/java/com/example/chat/SetupProfileActivity.kt
+++ app/src/main/java/com/example/chat/SetupProfileActivity.kt
@@ -1,11 +1,51 @@
 package com.example.chat
 
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import com.example.chat.databinding.ActivitySetupProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class SetupProfileActivity : AppCompatActivity() {
+
+    var binding: ActivitySetupProfileBinding? = null
+    var auth: FirebaseAuth? = null
+    var database: FirebaseDatabase? = null
+    var storage: FirebaseStorage? = null
+    var selectedImage: Uri? = null
+    var dialog: ProgressDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setup_profile)
+        binding = ActivitySetupProfileBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog!!.setMessage("Atualizando perfil")
+        dialog!!.setCancelable(false)
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        binding!!.imageProfile.setOnClickListener{
+            val intent = Intent ()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent,45)
+        }
+        binding!!.btConfirmProfile.setOnClickListener{
+            val name: String = binding!!.etInsertName.text.toString()
+            if (name.isEmpty()){
+                binding!!.etInsertName.setError("Por favor, digite nome")
+            }
+            dialog!!.show()
+            if (selectedImage !=null){
+                val reference = storage!!.reference.child("Perfil")
+                    .child(auth!!.uid!!)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/chat/model/User.kt
@@ -0,0 +1,4 @@
+package com.example.chat.model
+
+class User {
+}
