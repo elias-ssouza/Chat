8b7f5b97b25ba509c4bfa5aadcd2dddf3335cbbb
elias-ssouza <elias.ssouza@hotmail.com>
2022-11-24 15:40:53 -0300

    finalização da OTP Activity (integração com firebase)

--- app/src/main/java/com/example/chat/OTPActivity.kt
+++ app/src/main/java/com/example/chat/OTPActivity.kt
@@ -1,8 +1,11 @@
 package com.example.chat
 
 import android.app.ProgressDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.chat.databinding.ActivityOtpBinding
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
@@ -27,9 +30,9 @@ class OTPActivity : AppCompatActivity() {
         dialog!!.setCancelable(false)
         dialog!!.show()
         auth = FirebaseAuth.getInstance()
-        supportActionBar!!.hide()
+        supportActionBar?.hide()
         val phoneNumber = intent.getStringExtra("phoneNumber")
-        binding!!.tvPhone.text = "Verfique $phoneNumber"
+        binding!!.tvPhone.text = "Verifique $phoneNumber"
 
         val options = PhoneAuthOptions.newBuilder(auth!!)
             .setPhoneNumber(phoneNumber!!)
@@ -44,9 +47,34 @@ class OTPActivity : AppCompatActivity() {
                     TODO("Not yet implemented")
                 }
 
-                override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
-                    super.onCodeSent(p0, p1)
+                override fun onCodeSent(
+                    verifyId: String,
+                    forceResendingToken: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(verifyId, forceResendingToken)
+                    dialog!!.dismiss()
+                    verificationId = verifyId
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0)
+                    binding!!.otpView.requestFocus()
                 }
-            })
+            }).build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        binding!!.otpView.setOtpCompletionListener { otp ->
+            val credential = PhoneAuthProvider.getCredential(verificationId!!,otp)
+            auth!!.signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this@OTPActivity, SetupProfileActivity::class.java)
+                        startActivity(intent)
+                        finishAffinity()
+                    }else{
+                        Toast.makeText(this@OTPActivity,"Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+
+
     }
 }
