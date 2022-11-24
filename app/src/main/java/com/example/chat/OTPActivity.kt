d90700e9387e0301f812a350f74da8eba8ae34e8
elias-ssouza <elias.ssouza@hotmail.com>
2022-11-24 01:33:48 -0300

    OTP Activity

--- app/src/main/java/com/example/chat/OTPActivity.kt
+++ app/src/main/java/com/example/chat/OTPActivity.kt
@@ -1,11 +1,52 @@
 package com.example.chat
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chat.databinding.ActivityOtpBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class OTPActivity : AppCompatActivity() {
+
+    var binding : ActivityOtpBinding? = null
+    var verificationId: String? = null
+    var auth: FirebaseAuth? = null
+    var dialog: ProgressDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp)
+        binding = ActivityOtpBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog = ProgressDialog(this@OTPActivity)
+        dialog!!.setMessage("Enviando código...")
+        dialog!!.setCancelable(false)
+        dialog!!.show()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar!!.hide()
+        val phoneNumber = intent.getStringExtra("phoneNumber")
+        binding!!.tvPhone.text = "Verfique $phoneNumber"
+
+        val options = PhoneAuthOptions.newBuilder(auth!!)
+            .setPhoneNumber(phoneNumber!!)
+            .setTimeout(60L,TimeUnit.SECONDS)
+            .setActivity(this@OTPActivity)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(p0, p1)
+                }
+            })
     }
 }
--- app/src/main/java/com/example/chat/VerificationActivity.kt
+++ app/src/main/java/com/example/chat/VerificationActivity.kt
@@ -1,11 +1,35 @@
 package com.example.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chat.databinding.ActivityVerificationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class VerificationActivity : AppCompatActivity() {
+
+    var binding : ActivityVerificationBinding? = null
+
+    var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verification)
+        binding =ActivityVerificationBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        auth = FirebaseAuth.getInstance()
+        if(auth!!.currentUser != null){
+            val intent = Intent(this@VerificationActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        supportActionBar!!.hide()
+        binding!!.etEditNumber.requestFocus()
+        binding!!.btConfirmNumber.setOnClickListener {
+            val intent = Intent(this@VerificationActivity,OTPActivity::class.java)
+            intent.putExtra("phoneNumber",binding!!.etEditNumber.text.toString())
+            startActivity(intent)
+        }
+
     }
 }
