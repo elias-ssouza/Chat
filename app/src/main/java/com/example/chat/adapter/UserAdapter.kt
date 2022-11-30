a44279ea15e7798ef1208428f61f0db5f521a539
elias-ssouza <elias.ssouza@hotmail.com>
2022-11-30 01:41:06 -0300

    criação do adapter

---
+++ app/src/main/java/com/example/chat/adapter/UserAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.chat.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.chat.R
+import com.example.chat.databinding.ItemProfileBinding
+import com.example.chat.model.User
+
+class UserAdapter (var context:Context,var userList:ArrayList<User>):
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+
+    inner class UserViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+        val binding : ItemProfileBinding = ItemProfileBinding.bind(itemView)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        var v = LayoutInflater.from(context).inflate(R.layout.item_profile,parent,false)
+        return UserViewHolder(v)
+    }
+
+    override fun onBindViewHolder (holder: UserViewHolder, position: Int) {
+        val user = userList[position]
+        holder.binding.tvUsername.text = user.name
+        Glide.with(context).load(user.profileImage)
+            .placeholder(R.mipmap.user_avatar)
+            .into(holder.binding.ivProfile)
+    }
+
+    override fun getItemCount(): Int = userList.size
+}
