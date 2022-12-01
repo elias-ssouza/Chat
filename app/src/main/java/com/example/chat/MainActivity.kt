3880d4b3e253dd354fcea880492afa3a96fb57b6
elias-ssouza <elias.ssouza@hotmail.com>
2022-12-01 01:36:54 -0300

    construção da main activity

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.chat
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.chat.adapter.UserAdapter
+import com.example.chat.databinding.ActivityMainBinding
+import com.example.chat.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+    var database: FirebaseDatabase? = null
+    var users: ArrayList<User>? = null
+    var usersAdapter: UserAdapter? = null
+    var dialog: ProgressDialog? = null
+    var user: User? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog = ProgressDialog(this@MainActivity)
+        dialog!!.setMessage("Carregando imagem...")
+        dialog!!.setCancelable(false)
+        database = FirebaseDatabase.getInstance()
+        users = ArrayList<User>()
+        usersAdapter = UserAdapter(this@MainActivity, users!!)
+        val layoutManager = GridLayoutManager(this@MainActivity,2)
+        binding!!.rvMain.layoutManager = layoutManager
+        database!!.reference.child("users")
+            .child(FirebaseAuth.getInstance().uid!!)
+            .addValueEventListener(object :ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    user = snapshot.getValue(User::class.java)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+        binding!!.rvMain.adapter = usersAdapter
+        database!!.reference.child("users").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                users!!.clear()
+                for (snapshot1 in snapshot.children){
+                    val user:User? = snapshot1.getValue(User::class.java)
+                    if (!user!!.uid.equals(FirebaseAuth.getInstance().uid)) users!!.add(user)
+                }
+                usersAdapter!!.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val currentId = FirebaseAuth.getInstance().uid
+        database!!.reference.child("presence")
+            .child(currentId!!).setValue("Online")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val currentId = FirebaseAuth.getInstance().uid
+        database!!.reference.child("presence")
+            .child(currentId!!).setValue("Offline")
     }
 }
